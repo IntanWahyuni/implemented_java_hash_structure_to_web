10589de0aa27f7354852d5abba4ca417f824d814
Intan Wahyuni <intan.info@mhs.unsyiah.ac.id>
2020-06-06 22:06:27 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets\bootstrap\css\bootstrap.min.css">
+    <link rel="stylesheet" href="assets\css\fontawesome-free\css\all.min.css">
+    <link rel="stylesheet" href="assets\css\style.css">
+	<title>Data Editor</title>
+</head>
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg " style="background-color: #cc66ff">
+	  <a style="color: black" class="navbar-brand font-weight-bold" href="#"><i class="fas fa-edit"></i> DATA EDITOR</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item active">
+	        <a style="color: black" class="nav-link" href="cari.php">PENCARIAN EDITOR</a>
+	      </li>
+	      <li class="nav-item active">
+	        <a style="color: black" class="nav-link" href="tampil.php">TAMPILKAN EDITOR</a>
+	      </li>
+	      <li class="nav-item active">
+	        <a style="color: black"  class="nav-link" href="about.php">ABOUT ME</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+	<!-- End Navbar -->
+
+	<!-- Jumbotron -->
+	<div class="text-center mt-3">
+	  <h1 class="display-4">Assalammualaikum</h1>
+	  <p class="lead"> Website ini dibuat untuk menampilkan informasi editor pada artikel situs detik.com beserta artikel yang telah diedit<br>Data yang diperoleh berasal dari struktur data hash yang diintegrasi dengan Web</p>
+	  <hr class="my-4">
+	  <a class="btn btn-lg" href="" style ="background-color:#cc66ff"><i class="fab fa-github" style ="font-family: 'marck script',cursive;"></i> Visit My Github</a>
+	<!-- End Jumbotron -->
+
+	<script src="assets\bootstrap\js\jquery.min.js"></script>
+    <script src="assets\bootstrap\js\bootstrap.min.js"></script>
+
+</body>
+</html>
+
